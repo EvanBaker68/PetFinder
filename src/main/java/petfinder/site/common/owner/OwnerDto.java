170f6744afcd25f12d25eb681bbc16aa5b3f8493
Katy Atchison <katherine.atchison20@gmail.com>
2018-10-04 10:40:30 -0500

    Added OwnerDto

--- src/main/java/petfinder/site/common/owner/OwnerDto.java
+++ src/main/java/petfinder/site/common/owner/OwnerDto.java
@@ -1,4 +1,18 @@
 package petfinder.site.common.owner;
 
 public class OwnerDto {
+    private int ownerID;
+    private int sitterID;
+    //array of petIDs
+    //array of past bookings
+    //array of current bookings
+
+    public int getOwnerID() {
+        return ownerID;
+    }
+
+    public int getSitterID() {
+        return sitterID;
+    }
+
 }
