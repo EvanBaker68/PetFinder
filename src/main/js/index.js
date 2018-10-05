e34183cc859d078e5821445a00c8e021afb0ffab
Emily Tracey <Emily_Tracey@baylor.edyu>
2018-10-05 11:12:16 -0500

    syntax fix

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -7,6 +7,7 @@ export default class Index extends React.Component {
 
 	constructor(props){
 		super(props);
+
 		const cookies = new Cookies();
 		cookies.set('loggedIn', 'false', { path: '/' });
 
