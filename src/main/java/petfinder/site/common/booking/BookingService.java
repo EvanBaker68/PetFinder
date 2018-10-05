36ce3a216223e680759215be2ebb59b40fc21316
Kevin Bury <Andrew@Kevins-MacBook-Pro-2.local>
2018-10-04 19:49:47 -0500

    Added endpoints and services to Booking Owner and Sitter

--- src/main/java/petfinder/site/common/booking/BookingService.java
+++ src/main/java/petfinder/site/common/booking/BookingService.java
@@ -1,4 +1,20 @@
 package petfinder.site.common.booking;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import petfinder.site.common.pet.PetDto;
+
+import java.util.Optional;
+
 public class BookingService {
+    @Autowired
+    BookingDao bookingDao;
+
+    public Optional<BookingDto> findBooking(Long id) {
+        return bookingDao.findBooking(id);
+    }
+
+
+    public void save(BookingDto bookingDto) {
+        bookingDao.save(bookingDto);
+    }
 }
--- src/main/java/petfinder/site/common/owner/OwnerService.java
+++ src/main/java/petfinder/site/common/owner/OwnerService.java
@@ -1,4 +1,16 @@
 package petfinder.site.common.owner;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
 public class OwnerService {
+    @Autowired
+    OwnerDao ownerDao;
+
+    public Optional<OwnerDto> findOwner(String id){
+        return ownerDao.findOwner(id);
+    }
+
+    public void save(OwnerDto ownerDto){
+        ownerDao.save(ownerDto);
+    }
 }
