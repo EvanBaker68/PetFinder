ed117eefa5387364089160d7341a606e5163a6af
Katy Atchison <katherine.atchison20@gmail.com>
2018-10-07 13:21:10 -0500

    Took out a lot of User attributes- reverted mostly back to what the mentors had to make it work

--- src/main/java/petfinder/site/common/user/UserDto.java
+++ src/main/java/petfinder/site/common/user/UserDto.java
@@ -4,33 +4,55 @@ import org.codehaus.jackson.annotate.JsonIgnore;
 
 import alloy.util.Momento;
 
+import java.util.List;
+import java.util.Map;
+
 public class UserDto implements Momento<String> {
 	private String principal;
-	private String password;
 	private String phoneNumber;
 	private String name;
 	private String address;
+	private List<String> roles;
+	private UserType type;
+	private Map<String, Object> attributes;
 
 	private UserDto() {
 
 	}
 
-	public UserDto(String principal, String password, String phoneNumber, String name, String address) {
+	public UserDto(String principal, List<String> roles, UserType type, Map<String, Object> attributes) {
 	    setPrincipal(principal);
-	    setPassword(password);
+	    setRoles(roles);
+	    setAttributes(attributes);
+    }
+
+    //public UserDto(String principal, List<String> roles, UserType type, Map<String, Object> attributes, )
+
+	/*public UserDto(String principal, String phoneNumber, String name, String address) {
+	    setPrincipal(principal);
 	    setPhoneNumber(phoneNumber);
 	    setName(name);
 	    setAddress(address);
 
-	}
+	}*/
 
 	public String getPrincipal() {
 		String temp = principal;
 		return temp;
 	}
 
-    public String getPassword() {
-        return password;
+	public List<String> getRoles() {
+	    List<String> temp = roles;
+	    return temp;
+    }
+
+    public Map<String, Object> getAttributes() {
+	    return attributes;
+    }
+
+    public UserType getType() {
+	    UserType temp = type;
+	    return temp;
     }
 
     public String getPhoneNumber() {
@@ -55,12 +77,20 @@ public class UserDto implements Momento<String> {
 		this.principal = principal;
 	}
 
-    private void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+	public void setRoles(List<String> roles) {
+	    this.roles = roles;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setType(UserType type) {
+	    this.type = type;
+    }
+
+    public void setAttributes(Map<String, Object> attributes) {
+	    this.attributes = attributes;
+    }
+
+    private void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     public void setName(String name) {
@@ -69,5 +99,9 @@ public class UserDto implements Momento<String> {
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    public enum UserType {
+	    OWNER, SITTER
     }
 }
