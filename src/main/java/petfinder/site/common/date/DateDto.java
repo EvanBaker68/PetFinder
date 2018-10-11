cc694688b951138f560ea4fd907e18f948d29b31
Kevin Bury <Andrew@Kevins-MacBook-Pro-2.local>
2018-10-11 14:31:07 -0500

    Got it working :)

--- src/main/java/petfinder/site/common/date/DateDto.java
+++ src/main/java/petfinder/site/common/date/DateDto.java
@@ -1,18 +1,18 @@
 package petfinder.site.common.date;
 
 import alloy.util.Identifiable;
-
-
+import petfinder.site.ValidationException;
+import java.util.Date;
 public class DateDto implements Identifiable {
     private Long id;
     private String startDate;
     private String endDate;
     private String sitterPrinciple;
 
-    public DateDto(String startDate, String endDate, String sitterPrinciple) {
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.sitterPrinciple = sitterPrinciple;
+    public DateDto(String startDate, String endDate, String sitterPrinciple) throws ValidationException {
+        setStartDate(startDate);
+        setEndDate(endDate);
+        setSitterPrinciple(sitterPrinciple);
     }
 
     public DateDto() {
@@ -30,7 +30,11 @@ public class DateDto implements Identifiable {
         return startDate;
     }
 
-    public void setStartDate(String startDate) {
+    public void setStartDate(String startDate) throws ValidationException {
+        if(startDate == null){
+            throw new ValidationException("setStartDate", "given a null value");
+
+        }
         this.startDate = startDate;
     }
 
@@ -38,15 +42,24 @@ public class DateDto implements Identifiable {
         return endDate;
     }
 
-    public void setEndDate(String endDate) {
-        this.endDate = endDate;
+    public void setEndDate(String endDate) throws ValidationException {
+        if(endDate == null){
+            throw new ValidationException("setEndDate", " was set to null");
+
+        }        this.endDate = endDate;
     }
 
     public String getSitterPrinciple() {
         return sitterPrinciple;
     }
 
-    public void setSitterPrinciple(String sitterPrinciple) {
+    public void setSitterPrinciple(String sitterPrinciple) throws ValidationException{
+        if(sitterPrinciple == null){
+            throw new ValidationException("setSitterPrinciple", " set to null");
+        } else if(!sitterPrinciple.matches(".+\\@.+\\..+")){
+            throw new ValidationException("setSitterPrinciple", "did not match email regex");
+        }
+
         this.sitterPrinciple = sitterPrinciple;
     }
 }
