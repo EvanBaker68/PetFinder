6510a4b7b3e857996193d0cff2a6c4fa4cd26fdc
Kevin Bury <Andrew@Kevins-MacBook-Pro-2.local>
2018-11-13 14:33:38 -0600

    Added a shell sort off of rating and city

--- src/main/java/petfinder/site/strategy/ShellSort.java
+++ src/main/java/petfinder/site/strategy/ShellSort.java
@@ -2,24 +2,26 @@ package petfinder.site.strategy;
 
 import petfinder.site.common.sitter.SitterDto;
 
+import java.util.List;
+
 //NEED to use generics with this but have a copy with out it written down
 //text drew 5126946416 to get picture of algorithm without generics
 public class ShellSort {
 
 
-    boolean shellSort(SitterDto[] arr){
-        int n = arr.length;
+    public static List<SitterDto> shellSort(List<SitterDto> arr){
+        int n = arr.size();
         for(int gap = n / 2; gap > 0; gap /= 2){
             for(int i = gap; i < n; i++){
-                SitterDto temp = arr[i];
+                SitterDto temp = arr.get(i);
                 int j;
-                for(j = i; j >= gap && arr[j - gap].getRate() > temp.getRate(); j -= gap){
-                    arr[j] = arr[j - gap];
-                    arr[j] = temp;
+                for(j = i; j >= gap && arr.get(j - gap).getRate() > temp.getRate(); j -= gap){
+                    arr.set(i, arr.get(j - gap));
+                    arr.set(j, temp);
                 }
             }
         }
-        return true;
+        return arr;
     }
 
 
