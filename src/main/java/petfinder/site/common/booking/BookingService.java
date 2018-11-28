b6c564cce88ae9588b860ea7362e4116d810e5d3
Kevin Bury <Andrew@Kevins-MacBook-Pro-2.local>
2018-11-27 18:14:44 -0600

    Not working going to checkout another branch

--- src/main/java/petfinder/site/common/booking/BookingService.java
+++ src/main/java/petfinder/site/common/booking/BookingService.java
@@ -6,20 +6,23 @@ import petfinder.site.common.pet.PetDto;
 import petfinder.site.common.sitter.SitterDto;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class BookingService {
     @Autowired
     BookingDao bookingDao;
 
-    public Optional<BookingDto> findBooking(Long id) {
+    public BookingDto findBooking(Long id) {
         return bookingDao.findBooking(id);
     }
 
-    public List<Optional<BookingDto>> findBookingsBySitterPrincipal(String sitterPrincipal) {
+    public List<BookingDto> findBookingsBySitterPrincipal(String sitterPrincipal) {
         List<Optional<BookingDto>> theList =  bookingDao.findBookingBySitterPrincipal(sitterPrincipal);
+        //List<BookingDto> newList = theList.stream().filter(Optional::isPresent).collect(Collectors.toList());
         List<Optional<BookingDto>> newList = new ArrayList<>();
         for(Optional<BookingDto> booking : theList){
             if(!booking.get().getDeleted()){
