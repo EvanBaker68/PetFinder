895a9496e569b7ebe7fedf8342b4cf39f2adb7ef
Katy Atchison <katherine.atchison20@gmail.com>
2018-12-02 20:33:45 -0600

    Fixed pet card

--- src/main/js/components/pets/petCard.js
+++ src/main/js/components/pets/petCard.js
@@ -52,6 +52,25 @@ class PetCard extends React.Component{
 
 		return (
 			<Card className={classes.card}>
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        className={classes.media}
+                        height="180"
+                        image={Image}
+                        title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="headline" component="h2">
+                            {this.props.name}
+                            {console.log(this.props.id)}
+                        </Typography>
+                        <Typography component="p">
+                            {this.props.type}, {this.props.breed}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+
 				<CardActions>
 					<EditPet id={this.props.id}/>
 					<Button onClick={this.handleDelete} padding="10" color="secondary" variant="contained">
--- src/main/js/components/pets/petPage.js
+++ src/main/js/components/pets/petPage.js
@@ -4,27 +4,12 @@ import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
-import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import NotificationsIcon from '@material-ui/icons/Notifications';
 import MainListItems from 'js/components/dashboard/listItems';
 import PetCard from 'js/components/pets/petCard';
 import AddPet from 'js/components/pets/addPetForm';
-import CardActionArea from '@material-ui/core/CardActionArea/CardActionArea';
-import CardMedia from '@material-ui/core/CardMedia/CardMedia';
-import Image from 'js/images/dog1.jpeg';
-import CardContent from '@material-ui/core/CardContent/CardContent';
-import CardActions from '@material-ui/core/CardActions/CardActions';
-import EditPet from 'js/components/pets/editPetForm';
-import Button from '@material-ui/core/Button/Button';
-import Card from '@material-ui/core/Card/Card';
 import axios from 'axios';
 import Cookies from 'universal-cookie';
 import MenuBar from 'js/components/dashboard/MenuBar';
@@ -143,9 +128,6 @@ class PetPage extends React.Component {
 			.catch(error => this.setState({error}));
     }
 
-
-
-
     handleDrawerOpen = () => {
         this.setState({ open: true });
     };
@@ -173,22 +155,11 @@ class PetPage extends React.Component {
 			}).then(response => console.log(response))
 			.catch(error => this.setState({error}));
 		this.handler();
-        // console.log(this.state.reload);
-        // if(this.state.reload === false) {
-        //     console.log('unfofrtdsa');
-			// this.setState({ reload: true });
-			// console.log(this.state.reload);
-        // }
-        // else
-			// this.setState({ reload: false });
-        // console.log('HEYYYYY');
     }
 
     render() {
         const { classes } = this.props;
         const { pets } = this.state;
-
-        console.log('Reload: ', this.state.reload);
 
         var petItems;
         if(pets)
@@ -224,18 +195,9 @@ class PetPage extends React.Component {
                         <MainListItems/>
                     </Drawer>
                     <main className={classes.content}>
-                        {/*<Card className={classes.card}>*/}
-                            {/*<CardActions>*/}
-                                {/*<EditPet/>*/}
-                            {/*</CardActions>*/}
-                        {/*</Card>*/}
                         {petItems}
 						<div className={classes.divider}/>
-
-							<AddPet handler={this.stateChangeHandler}/>
-
-                        {/*<PetCard/>*/}
-
+                        <AddPet handler={this.stateChangeHandler}/>
                     </main>
                 </div>
             </React.Fragment>
