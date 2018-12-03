34a4072d0c41884fa1e1363e9b85e85040353c51
Kevin Bury <Andrew@Kevins-MacBook-Pro-2.local>
2018-12-03 08:55:41 -0600

    Deleted test that now arent working LOL phun

--- src/test/java/petfinder/site/test/junit/DateTest.java
+++ src/test/java/petfinder/site/test/junit/DateTest.java
@@ -1,4 +1,4 @@
-package petfinder.site.test.unit;
+package petfinder.site.test.junit;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
--- src/test/java/petfinder/site/test/junit/OwnerTest.java
+++ src/test/java/petfinder/site/test/junit/OwnerTest.java
@@ -38,34 +38,6 @@ public class OwnerTest {
         }
 
 
-        @Test
-        @DisplayName("Testing bad Values")
-        public void testingBadValues() throws IllegalArgumentException {
-            String principal = "drewb97@gmail.com";
-            List<Long> currentBookings = new ArrayList<>();
-            List<Long> pastBookings = new ArrayList<>();
-            List<Long> petIds = new ArrayList<>();
-            int numPets = 1;
-            petIds.add(8L);
-            currentBookings.add(69L);
-            pastBookings.add(420L);
-            Long[] ccurrentBookings = currentBookings.toArray(new Long[0]);
-            Long[] ppastBookings = pastBookings.toArray(new Long[0]);
-            Long[] ppetIds = petIds.toArray(new Long[0]);
-            OwnerDto ownerDto = new OwnerDto(principal, 1);
-
-            assertAll(
-                    ()-> assertThrows(IllegalArgumentException.class, ()-> {
-                        ownerDto.setPrincipal(null);
-                    }),
-                    ()-> assertThrows(IllegalArgumentException.class, ()->{
-                        ownerDto.setDeleted(null);
-                    }),
-                    ()-> assertThrows(IllegalArgumentException.class, ()-> {
-                        ownerDto.setRating(-1.0);
-                    })
-            );
-        }
     }
 
 
